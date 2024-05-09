f45c9b52d59c9eaaa8c94e3c717efff85e60f8cd
chupengfei <chupengfeiit@gmail.com>
2024-05-09 15:34:03 +0800

    feat: add dynamic data

--- src/pages/Asset/index.tsx
+++ src/pages/Asset/index.tsx
@@ -1,14 +1,20 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Introduce from './components/Introduce'
 import DataTable from './components/DataTable'
 
 export default function Asset() {
+  const [pnlVal, setPnlVal] = useState<string | number>('23.11')
+  const [totalVal, setTotalVal] = useState<string | number>('1233.21')
+
   useEffect(() => {
-    // TODO: fetch data from interface
+    // TODO: fetch data from interface & update the pnlVal
+    setPnlVal('23.11')
+    setTotalVal('1233.21')
   }, [])
+
   return (
     <>
-      <Introduce />
+      <Introduce pnlVal={pnlVal} totalVal={totalVal} />
       <DataTable />
     </>
   )
