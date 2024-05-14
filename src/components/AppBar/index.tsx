cd95d5d7b1893751303eb60fc7df735dc0617088
Max <hi.max@foxmail.com>
2024-05-14 17:19:31 +0800

    feat: add X logo

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,20 +1,19 @@
 import LogoBoxSvg from '../../assets/images/logo-box.svg'
-import LogoTextSvg from '../../assets/images/logo-text.svg'
 import NavTabs from '../NavTabs'
 import LinkTab from '../LinkTab'
 import { Link } from 'react-router-dom'
 import Web3Status from '../Web3Status'
+import XLogo from '@/assets/images/X-logo.svg'
 
 export default function AppBar() {
   return (
     <header className={'flex h-[80px] flex-row justify-center bg-[#030303] px-[56px]'}>
       <div className={'flex w-full max-w-[1684px] flex-row items-center'}>
         <Link to={'/'} className={'mr-10'}>
-          <h1 className={'sr-only'}>{'PREOTC'}</h1>
-          <div className={'flex items-center gap-2'}>
+          <h1 className={'flex items-center gap-2'}>
             <img src={LogoBoxSvg} alt={''} />
-            <img src={LogoTextSvg} alt={''} />
-          </div>
+            <span className={'text-[25px] text-lemonYellow'}>PACKEX</span>
+          </h1>
         </Link>
 
         <NavTabs>
@@ -24,7 +23,13 @@ export default function AppBar() {
           <LinkTab to={'/pex'}>{'$PEX'}</LinkTab>
         </NavTabs>
 
-        <div className={'ml-auto'}>
+        <div className={'ml-auto flex items-center gap-6'}>
+          <a
+            href={'https://twitter.com/packex_io'}
+            className={'flex h-7 w-7 items-center justify-center rounded border border-white/50'}
+          >
+            <img src={XLogo} alt="" />
+          </a>
           <Web3Status />
         </div>
       </div>
