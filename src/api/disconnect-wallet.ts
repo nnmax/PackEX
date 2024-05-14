9a07f9d7c38d7830f3f822967ac11c21b4b17c2f
Max <hi.max@foxmail.com>
2024-05-14 22:57:36 +0800

    feat: connect to Wallet with API

---
+++ src/api/disconnect-wallet.ts
@@ -0,0 +1,12 @@
+import { MESSAGE_KEY, SIGNATURE_KEY, USER_KEY } from '@/constants'
+import fetcher from '@/utils/fetcher'
+
+export default function disconnectWallet() {
+  window.localStorage.removeItem(USER_KEY)
+  window.localStorage.removeItem(MESSAGE_KEY)
+  window.localStorage.removeItem(SIGNATURE_KEY)
+
+  return fetcher('/api/disconnect-wallet', {
+    method: 'POST',
+  })
+}
