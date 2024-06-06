263e05df1dcc04f5397f6d2965f2934236fe6f53
Max <hi.max@foxmail.com>
2024-06-06 17:35:12 +0800

    fix: remove url

--- src/pages/Pool/my/index.tsx
+++ src/pages/Pool/my/index.tsx
@@ -94,7 +94,7 @@ const PoolMy = () => {
                       {'+ADD'}
                     </Link>
                     <Link
-                      to={`/pool/${item.token0Contract}/eth/${item.token1Contract}`}
+                      to={`/pool/remove/${item.token0Contract}/${item.token1Contract}`}
                       className={
                         'text-lemonYellow w-[88px] flex items-center justify-center h-6 border rounded-sm border-lemonYellow'
                       }
